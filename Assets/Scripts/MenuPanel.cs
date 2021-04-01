5103b9fe2b7b9df8a0dfb27606925f75442ad337
ShiningBreaD <bordakovvbros@gmail.com>
2021-04-01 16:32:04 +0900

    Tiny fix

--- Assets/Scripts/MenuPanel.cs
+++ Assets/Scripts/MenuPanel.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuPanel : MonoBehaviour
 {
+    public Text title;
+
     public void Statistics(GameObject panel)
     {
         panel.SetActive(!panel.activeSelf);
+        title.text = "Statistics";
     }
 
     public void Shop(GameObject panel)
     {
         panel.SetActive(!panel.activeSelf);
+        title.text = "Statistics";
     }
 
     public void Settings(GameObject panel)
