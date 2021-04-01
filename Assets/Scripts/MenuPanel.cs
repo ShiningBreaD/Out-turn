aa177e4fa565c3a2d1d2a595dfd02073475ae347
ShiningBreaD <bordakovvbros@gmail.com>
2021-04-01 16:27:43 +0900

    Added menu prototype

---
+++ Assets/Scripts/MenuPanel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MenuPanel : MonoBehaviour
+{
+    public void Statistics(GameObject panel)
+    {
+        panel.SetActive(!panel.activeSelf);
+    }
+
+    public void Shop(GameObject panel)
+    {
+        panel.SetActive(!panel.activeSelf);
+    }
+
+    public void Settings(GameObject panel)
+    {
+        panel.SetActive(!panel.activeSelf);
+    }
+}
